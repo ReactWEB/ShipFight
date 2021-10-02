5bb21b2b2ecfaac24b13c9baf188f6a77c059213
ReactWEB <reactweb@ukr.net>
2021-10-02 14:11:42 +0300

    shipping fight

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Уровень 1) Есть два корабля, первый корабль имеет  HP (Health Points) 100, Damage 4, второй корабль имеет HP 40, damage 15. Описать объектами.
+//В одной итерации стреляют друг в друга. Вывести лог боя. Определить победителя.
+
+
+const ship1 = {health: 100, damage: 4,};
+const ship2 = {health: 40, damage: 15,};
+let a = ship1.health;
+let b = ship2.health;
+
+const fight = () => {
+  let z = 0;
+  while (z < 7) {
+    a -= ship2.damage;
+    b -= ship1.damage;    
+    z++;
+  }
+  if (a > b) {
+    console.log('The ship1 is winner!');
+  } else {
+    console.log('The ship2 is winner!');
+  }
+}
+fight();
