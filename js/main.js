cadfa6537813321dcad334d833313cb8bd930e9c
ReactWEB <reactweb@ukr.net>
2021-10-02 14:19:54 +0300

    working

--- js/main.js
+++ js/main.js
@@ -7,12 +7,10 @@ const ship2 = {health: 40, damage: 15,};
 let a = ship1.health;
 let b = ship2.health;
 
-const fight = () => {
-  let z = 0;
-  while (z < 7) {
+const fight = () => {  
+  while (a > 0 || b > 0) {
     a -= ship2.damage;
     b -= ship1.damage;    
-    z++;
   }
   if (a > b) {
     console.log('The ship1 is winner!');
